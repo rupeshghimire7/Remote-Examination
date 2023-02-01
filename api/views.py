febe792b43f2797d0ebb502c0a3fe69ba4addbcd
Rupesh Ghimire <rupeshghimire007@gmail.com>
2023-02-01 19:56:36 +0545

    API app started and created test view

---
+++ api/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+
+# Create your views here.
+
+def api_home(request, *args, **kwargs):
+    return JsonResponse({'query':"This is api response!!"})
