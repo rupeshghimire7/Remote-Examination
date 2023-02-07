f86ec450ee2ca1245c27c946cef6daf5a573bd3a
Rupesh Ghimire <rupeshghimire007@gmail.com>
2023-02-07 08:39:22 +0545

    Starting Serializers and serializer view on own

--- api/serializers.py
+++ api/serializers.py
@@ -1,20 +1,5 @@
 from rest_framework import serializers
 from exam.models import *
 
-
-class QuestionSerializer(serializers.ModelSerializer):
-    question = serializers.SerializerMethodField(read_only = True)
-    class Meta:
-        model = Question
-        fields = [
-            'id',
-            'correct',
-            'points',
-            # 'qn',
-            'question',
-        ]
-    def get_question(self,obj):
-        return obj.get_question()   #this get_question is a method in models of exam Question
-
-        
+       
 
--- api/views.py
+++ api/views.py
@@ -1,41 +1,38 @@
-from django.forms.models import model_to_dict
-
 from .serializers import QuestionSerializer
 from exam.models import *
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import generics
-# Create your views here.
 
 
-# @api_view(["POST"])
-# def api_home(request, *args, **kwargs):
+# # @api_view(["POST"])
+# # def api_home(request, *args, **kwargs):
 
-#     instance = Question.objects.order_by("?").first()
-#     data = {}
-#     if instance:
-#         data = QuestionSerializer(instance).data
-#     # if model_question:
-#     #     data = model_to_dict(model_question, fields=['id','qn','correct'])
+# #     instance = Question.objects.order_by("?").first()
+# #     data = {}
+# #     if instance:
+# #         data = QuestionSerializer(instance).data
+# #     # if model_question:
+# #     #     data = model_to_dict(model_question, fields=['id','qn','correct'])
 
-#     return Response(data) 
-
-
-class QuestionAPIView(generics.RetrieveAPIView):
-    queryset = Question.objects.all()
-    serializer_class = QuestionSerializer
-    #it has pk as lookup field
-
-# question_view = QuestionAPIView.as_view()
+# #     return Response(data) 
 
 
-class QuestionListCreateAPIView(generics.ListCreateAPIView):
-    queryset = Question.objects.all()
-    print(queryset)
-    serializer_class = QuestionSerializer
+# class QuestionAPIView(generics.RetrieveAPIView):
+#     queryset = Question.objects.all()
+#     serializer_class = QuestionSerializer
+#     #it has pk as lookup field
 
-    def perform_create(self, serializer):
-        # serializer.save(user=self.request.user)
-        serializer.save()
-        print(serializer)
+# # question_view = QuestionAPIView.as_view()
+
+
+# class QuestionListCreateAPIView(generics.ListCreateAPIView):
+#     queryset = Question.objects.all()
+#     print(queryset)
+#     serializer_class = QuestionSerializer
+
+#     def perform_create(self, serializer):
+#         # serializer.save(user=self.request.user)
+#         serializer.save()
+#         print(serializer)
