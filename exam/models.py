c3c732afe974d482e5533dce149376a3d86b5474
Rupesh Ghimire <rupeshghimire007@gmail.com>
2023-02-20 07:50:57 +0545

    College and Subject models added

--- exam/models.py
+++ exam/models.py
@@ -50,4 +50,20 @@ class Student(models.Model):
 
 
     def __str__(self):
+        return self.name\
+
+
+class Subject(models.Model):
+    subject = models.CharField(max_length=50,null=True,blank=False)
+    # SubCode = models.CharField(max_length=50,null=True)
+
+    def __str__(self):
         return self.name
+
+
+class College(models.Model):
+    college = models.CharField(max_length=200,null=True,blank=False)
+    code = models.CharField(max_length=5,null=True,blank=False)
+
+    def __str__(self):
+        return f"{self.code} ----->>  {self.college} "
